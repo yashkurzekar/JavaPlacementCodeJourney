8a05d4d87beca007c2291f6de6825ebc843b0021
yash kurzekar <yashkurzekar2@gmail.com>
2022-06-02 23:02:58 +0530

    Scanner input

--- src/javaBasic/userInput.java
+++ src/javaBasic/userInput.java
@@ -9,6 +9,11 @@ public class userInput {
         System.out.println("Enter your age");
         int a = sc.nextInt();
         System.out.println("Your age is "+a);
+        System.out.println("Enter Your Name");
+        sc.nextLine();//When we take any input before nextLine then it hit enter automatically and move so we use dummy sc object to get proper input
+        String name = sc.nextLine();
+    System.out.println("Your Name is "+ name);
+
 
     }
 }
