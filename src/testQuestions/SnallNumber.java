9af8d13948a690a52ac6e89b199826d396c2d8ec
yash kurzekar <yashkurzekar2@gmail.com>
2022-06-05 22:17:27 +0530

    Small Number

---
+++ src/testQuestions/SnallNumber.java
@@ -0,0 +1,26 @@
+//package testQuestions;
+//
+//import java.util.Scanner;
+//
+//public class SnallNumber {
+//    public static void main(String[] args) {
+//        Scanner sc =new Scanner(System.in);
+//        System.out.println("Enter Size of an array");
+//        int size = 0;
+//        int value=0;
+//        int arr[] = new int [size];
+//        System.out.println("Enter elements in array");
+//        for (int i=0;i<size;i++) {
+//            arr[i]= sc.nextInt();
+//        }
+//        for(int i=0;i<size;i++) {
+//            public static int smallest()
+//            {
+//                if (value>arr[i]){
+//                    value=arr[i];
+//                }
+//            }
+//
+//        }
+//    }
+//}
