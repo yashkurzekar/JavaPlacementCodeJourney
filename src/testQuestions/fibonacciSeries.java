3cb37ff8f897401750f80ae25d5a4dff24a9f1fb
yash kurzekar <yashkurzekar2@gmail.com>
2022-06-06 22:22:26 +0530

    Bugs resolver

--- src/testQuestions/fibonacciSeries.java
+++ src/testQuestions/fibonacciSeries.java
@@ -14,7 +14,7 @@ public class fibonacciSeries {
         }
 
     }
-    public static void main(String args[]){
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("how many fib series you wanted to print");
         int count= sc.nextInt();
