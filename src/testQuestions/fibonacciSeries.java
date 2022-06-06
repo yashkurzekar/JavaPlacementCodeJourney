3cd8bb3262e776e1131fd8812ac62da2b5ab6e40
yash kurzekar <yashkurzekar2@gmail.com>
2022-06-06 21:18:35 +0530

    Fib

--- src/testQuestions/fibonacciSeries.java
+++ src/testQuestions/fibonacciSeries.java
@@ -12,12 +12,15 @@ public class fibonacciSeries {
             System.out.print(" "+n3);
             printFibonacci(count-1);
         }
+
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("how many fib series you wanted to print");
         int count= sc.nextInt();
         System.out.print(n1+" "+n2);//printing 0 and 1
+        System.out.println("Thank You");
         printFibonacci(count-2);//n-2 because 2 numbers are already printed
+
     }
 }
