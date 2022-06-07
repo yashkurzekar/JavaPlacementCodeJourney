2b8ae28b05e36a244e26631ed9d3c260f63d078f
yash kurzekar <yashkurzekar2@gmail.com>
2022-06-07 20:05:17 +0530

    Harshu code

---
+++ src/javaBasic2/Harshu.java
@@ -0,0 +1,8 @@
+package javaBasic2;
+
+public class Harshu {
+    public static void main(String[] args) {
+        System.out.println("Hello Harshu");
+        System.out.println("How are you?");
+    }
+}
