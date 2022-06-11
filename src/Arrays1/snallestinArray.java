e492c782840ba79ab6dcc27e4add52fd4af4b5d4
yash kurzekar <yashkurzekar2@gmail.com>
2022-06-11 23:55:23 +0530

    buggy code

---
+++ src/Arrays1/snallestinArray.java
@@ -0,0 +1,30 @@
+package Arrays1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class snallestinArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an array");
+        int size = sc.nextInt();
+        int arr [] = new int[size];
+        int value = 0;
+        for(int i=0;i<size;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+
+        for (int i=0;i<size;i++){
+            for (int j=i+1;j<=size;j++){
+                if (arr[i] > arr[j]) {
+                    value = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = value;
+                }
+            }
+
+        }
+        System.out.println("Smallest element is "+arr[0]);
+    }
+}
