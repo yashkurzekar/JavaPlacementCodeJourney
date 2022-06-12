dea9fc39fbab6ce82dfef723d407561756aac8c7
yash kurzekar <yashkurzekar2@gmail.com>
2022-06-12 22:35:38 +0530

    sws

--- src/Arrays1/removeDuplicate.java
+++ src/Arrays1/removeDuplicate.java
@@ -1,24 +1,28 @@
 package Arrays1;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class removeDuplicate {
-    public static void duplictae(int[] a)
-    {
-        int j=0;
-        for (int i=0;i<a.length;i++)
-        {
-            if(a[i]!=a[j])
-            {
-                a[j+1]=a[i];
-                //System.out.print(" "+a[i]);
-                j++;
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int temp;
+        System.out.println("Enter the size of an array");
+        int size = sc.nextInt();
+        int array[] = new int[size];
+        System.out.println("Enter the elements in the array");
+        for (int i=0;i<size;i++) {
+            array[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i<size; i++ ){
+            for(int j = i+1; j<size; j++){
+                if(array[i]>array[j]){
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
             }
         }
-    }
-    public static void main(String[] args) {
-        int a[] ={1,2,2,3,4,5,5,5,6};
-        duplictae(a);
-        System.out.println(Arrays.toString(a)); //print whole array
+        System.out.println("Smallest element of the array is:: "+array[0]);
     }
 }
