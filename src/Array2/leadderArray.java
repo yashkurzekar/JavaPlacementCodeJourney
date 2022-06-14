5e7aa2071d86ab15e361f02f7beb196774a852f2
yash kurzekar <yashkurzekar2@gmail.com>
2022-06-14 20:05:21 +0530

    Leader in array

--- src/Array2/leadderArray.java
+++ src/Array2/leadderArray.java
@@ -4,17 +4,19 @@ import java.util.Arrays;
 //Big O(n^2) worst case
 public class leadderArray {
     public static void main(String[] args){
-        int array[] = {2,7,6,1,4,3 };
+        int array[] = {2,7,6,1,4,3};
         System.out.println(Arrays.toString(array));
+        System.out.println(array.length+" is length of array");
         System.out.println("those are the leader");
         loop1:
-        for (int i = 0;i< array.length;i++) {
-            for (int j=i;j<array.length;j++){
-                if(array[j]< array[i])
+        for (int i = 1;i< array.length;i++) {
+            for (int j=0;j<array.length;j++){
+                if(array[j] < array[i])
                 {
+                   // System.out.println(array[j]);
                     System.out.print(" "+array[i]);
-                    //break;
-                    continue loop1;
+                    break;
+                    //continue loop1;
                 }
             }
         }
