9175fa50fca175e70e52cda7804958022c8a6ebf
yash kurzekar <yashkurzekar2@gmail.com>
2022-06-14 21:09:37 +0530

    Largest Sum of sub array

---
+++ src/Array2/krimsAlgoritham.java
@@ -0,0 +1,20 @@
+package Array2;
+
+public class krimsAlgoritham {
+    static  int largestSumSubArray(int a[]){
+        int current = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0;i<a.length;i++){
+            current+=a[i];
+            if (max < current) max = current;
+            if (current<0) current = 0;
+        }
+        return max;
+    }
+    //main
+    public static void main(String[] args) {
+        int a[] = {-2,7,-6,4,1,-3,8};
+        System.out.println(largestSumSubArray(a));
+
+    }
+}
