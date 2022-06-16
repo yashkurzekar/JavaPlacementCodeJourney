9afbba9597777e1bf6f73cf3ec9015070a4cc47d
yash kurzekar <yashkurzekar2@gmail.com>
2022-06-16 12:53:19 +0530

    Find if there is a subarray with Sum 0

---
+++ src/Array3/PrefixSum.java
@@ -0,0 +1,26 @@
+package Array3;
+//Find if there is a subarray with Sum 0
+import java.util.HashSet;
+import java.util.Set;
+
+public class PrefixSum {
+
+    static boolean subArrayWithZeroSum(int [] a) {
+        int n = a.length;;
+        Set<Integer> set = new HashSet<>(); //created set
+        int sum=0;
+        for (int i=0;i<n;i++) {
+            sum+=a[i];
+            if (set.contains(sum)){
+                return true;
+            }
+            set.add(sum);
+        }
+        return false;
+
+    }
+    public static void main(String[] args) {
+     int a[] = {2,3,1,-4,3,-2};
+        System.out.println(subArrayWithZeroSum(a));
+    }
+}
